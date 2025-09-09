9bd91039b11f387fd2328269f813142fee9984db
thathvik <tarunthathvik@gmail.com>
2025-09-09 16:00:01 -0400

    Improved the base api client with response status code check and used the inbuilt HTTPMethod class to pass GET and POST methods.

--- src/api_clients/base_api_client.py
+++ src/api_clients/base_api_client.py
@@ -1,10 +1,10 @@
 import requests
 
+from http import HTTPMethod
 from typing import Optional, Any, Union, SupportsFloat
-from requests import Response
 from urllib.parse import urljoin
 
-from src.constants import STR_GET, STR_POST, LIST_SUPPORTED_HTTPS_METHODS
+from requests import Response
 
 from src.logger import get_logger
 
@@ -20,21 +20,27 @@ class BaseApiClient:
         # Default (uniform for all calls)
         self.headers    = {}
         self.params     = {}
+        self.LIST_SUPPORTED_HTTP_METHODS = [HTTPMethod.GET, HTTPMethod.POST]
     
 
-    def rest_call(self, http_method: str, endpoint: Optional[str] = None, headers: Optional[dict]= None, params: Optional[dict]= None, payload: Optional[Any]= None):
+    def rest_call(self, http_method: HTTPMethod, endpoint: Optional[str] = None, headers: Optional[dict]= None, params: Optional[dict]= None, payload: Optional[Any]= None):
         logger.debug(f"+rest_call()")
-        assert isinstance(http_method, str) and len(http_method) > 0, f"rest_call(): Invalid {type(http_method)=}"
+        assert isinstance(http_method, HTTPMethod), f"rest_call(): Invalid {type(http_method)=}, excepted 'HTTPMethod'"
 
         url = urljoin(self.base_url, endpoint)
         
         headers = self.headers if headers is None else self.headers | headers
         params  = self.params if params is None else self.params | params
 
-        if http_method in LIST_SUPPORTED_HTTPS_METHODS:
-            response = self.get(url, params, headers) if http_method == STR_GET else self.post(url, params, headers, payload)
-        else:
-            msg = f"rest_call(): {http_method=} is not one of the expected values '{LIST_SUPPORTED_HTTPS_METHODS}'"
+        # Here, let's use a pythonic way of returning an exception.
+        if http_method not in self.LIST_SUPPORTED_HTTP_METHODS:
+            msg = f"rest_call(): {http_method=} is not one of the expected values '{self.LIST_SUPPORTED_HTTP_METHODS}'"
+            logger.error(msg)
+            raise ValueError(msg)
+        
+        response = self.get(url, params, headers) if http_method == HTTPMethod.GET else self.post(url, params, headers, payload)
+        if not response.ok:
+            msg = f"rest_call(): API returned with a status code '{response.status_code}':'{response.reason}'."
             logger.error(msg)
             raise ValueError(msg)
         
