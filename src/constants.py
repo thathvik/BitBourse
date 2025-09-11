2a380fe730b18e71c21a24f089352371b6a1925f
thathvik <tarunthathvik@gmail.com>
2025-09-10 23:18:18 -0400

    Implemented Stocks API code based on documentation.

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG').upper()
 
+ALPHA_VATAGE_API_KEY = os.environ.get('ALPHA_VATAGE_API_KEY','')
+
 STR_GET     = "GET"
 STR_POST    = "POST"
 
@@ -12,3 +14,13 @@ LIST_SUPPORTED_HTTPS_METHODS = [STR_GET, STR_POST]
 
 STR_FREE    = "FREE"
 STR_PREMIUM = "PREMIUM"
+
+# Stock API related constants
+ALPHA_VANTAGE_API_URL           = "https://www.alphavantage.co/"
+ALPHA_VANTAGE_API_KEY_PARAM     = "apikey"
+ALPHA_VATAGE_API_QUERY_ENDPOINT = "query"
+
+STR_TIME_SERIES_DAILY   = "TIME_SERIES_DAILY"
+
+TIME_SERIES_DAILY_KEY   = "Time Series (Daily)"
+CLOSE_KEY               = "4. close"
